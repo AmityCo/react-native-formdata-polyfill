89843e3ea1b3c41bed6e3afc3df9149f6c8c46a8
Reza Babaei <ramyariran@gmail.com>
2021-12-28 12:12:01 +0700

    fix: change main entry to inde.ts

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,3 @@
-declare let navigator:
-  | {
-      [k: string]: any;
-    }
-  | undefined;
+import polyfill from './polyfill';
 
-// from @types/react-native
-declare type FormDataPart =
-  | {
-      string: string;
-      headers: { [name: string]: string };
-    }
-  | {
-      uri: string;
-      headers: { [name: string]: string };
-      name?: string;
-      type?: string;
-    };
-
-declare class FormData {
-  // eslint-disable-next-line no-unused-vars
-  append(name: string, value: any): void;
-
-  getParts(): Array<FormDataPart>;
-
-  getAll(): Array<FormDataPart>;
-}
-
-export default function polyfill() {
-  if (navigator?.product === 'ReactNative' && FormData && !FormData?.prototype?.getAll) {
-    FormData.prototype.getAll = FormData.prototype.getParts;
-  }
-}
+polyfill();
--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,3 +1,33 @@
-import polyfill from '.';
+declare let navigator:
+  | {
+      [k: string]: any;
+    }
+  | undefined;
 
-polyfill();
+// from @types/react-native
+declare type FormDataPart =
+  | {
+      string: string;
+      headers: { [name: string]: string };
+    }
+  | {
+      uri: string;
+      headers: { [name: string]: string };
+      name?: string;
+      type?: string;
+    };
+
+declare class FormData {
+  // eslint-disable-next-line no-unused-vars
+  append(name: string, value: any): void;
+
+  getParts(): Array<FormDataPart>;
+
+  getAll(): Array<FormDataPart>;
+}
+
+export default function polyfill() {
+  if (navigator?.product === 'ReactNative' && FormData && !FormData?.prototype?.getAll) {
+    FormData.prototype.getAll = FormData.prototype.getParts;
+  }
+}
